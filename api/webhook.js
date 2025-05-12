684eac0aff2a821ded242742dd1c9babb2eeae54
richmice <richmice555@gmail.com>
2025-05-12 17:25:00 +0800

    Add files via upload

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-module.exports = async (req, res) => {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
